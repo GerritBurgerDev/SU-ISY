f7b17b294e0baefd4816a166cbb636819fcdf7e6
GerritBurgerDev <gerrit.burger777@gmail.com>
2020-11-02 23:00:34 +0200

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gerritburgerdev/'
+    : '/',
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
